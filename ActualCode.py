359d4af639cc6f1ea771004eb6cc0ac944b6259f
DarkEquinox564 <58512908+DarkEquinox564@users.noreply.github.com>
2020-04-08 19:26:42 +0530

    Create ActualCode.py

---
+++ ActualCode.py
@@ -0,0 +1,20 @@
+# Finder
+Lets You find The number of letters
+
+def main():
+
+  nanme = input('Enter a Name: ').lower()
+  
+  lett = input('What Letter : ')
+
+  count = 0
+
+  for c in nanme:
+    if c == lett:
+      count +=1
+  print(count)     
+
+  
+
+
+main()  
